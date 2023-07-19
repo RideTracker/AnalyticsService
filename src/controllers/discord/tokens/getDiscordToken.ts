f041dcb909ebc582d9181a5f66fc4dc739ea7e06
Nora Söderlund <norasoderlund@icloud.com>
2023-07-19 22:45:07 +0200

    Fixed getDiscordToken

--- src/controllers/discord/tokens/getDiscordToken.ts
+++ src/controllers/discord/tokens/getDiscordToken.ts
@@ -1,6 +1,6 @@
 export default async function getDiscordToken(env: Env) {
     const authentication = `${env.DISCORD_BOT_CLIENT_ID}:${env.DISCORD_BOT_CLIENT_TOKEN}`;
-    const base64AuthString = Buffer.from(authentication, "base64");
+    const base64AuthString = Buffer.from(authentication, "base64url");
 
     const response = await fetch("https://discord.com/api/v9/oauth2/token", {
         method: "POST",
